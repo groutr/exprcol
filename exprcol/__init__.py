ed3f93884d3224df0027808c479fba5a13c0aac4
Ryan Grout <ryan@ryangrout.org>
2019-10-31 13:31:25 -0500

    Remove check function.

--- exprcol/__init__.py
+++ exprcol/__init__.py
@@ -3,6 +3,7 @@ __all__ = ('register', 'requires')
 from collections.abc import Sequence
 from operator import itemgetter
 from functools import wraps
+import inspect
 
 REGISTRY = {}
 
@@ -10,24 +11,9 @@ requires = itemgetter(slice(1, None))
 
 def register(name, requires):
     def decorator(func):
-        if _check_function(func, requires):
+        if callable(func):
             REGISTRY[name] = (func, *requires)
-        return func
-    return decorator
-
-
-def _check_function(func, args):
-    """Check to see if function has right arity
-
-    Arguments:
-        func {[type]} -- [description]
-        args {[type]} -- [description]
-    """
-    if callable(func):
-        argcount = func.co.code_argcount
-        if isinstance(args, Sequence):
-            return argcount == len(args)
+            return func
         else:
-            raise TypeError(f"args must be a sequence")
-    else:
-        raise TypeError("function not callable")
+            raise TypeError("func is not callable")
+    return decorator
